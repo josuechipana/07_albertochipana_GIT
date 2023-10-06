a3f5a165d82190428bcf8d06f41250e40b643d5d
albertochipana <alberto.chipana@vallegrande.edu.pe>
2023-10-06 07:03:30 -0500

    java

---
+++ 07_CHIPANAMONTES/src/S07_07/Main_S07_07.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S07_07;
+
+/**
+ *
+ * @author chipa
+ */
+public class Main_S07_07 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO");
+    }
+
+    
+}
---
+++ 07_CHIPANAMONTES/src/S07_07/person1_07_07.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S07_07;
+
+/**
+ *
+ * @author chipa
+ */
+public class person1_07_07 {
+    
+}
